037d498bc6c20e0ea1a3455e5438b49afbaa4b65
saralove20 <saralove20@naver.com>
2026-02-18 22:38:44 +0900

    [Feat] 라우팅 설정 및 컨트롤러의 객체 관리를 한 곳에서 관리하도록 구현
    - AppConfig에서 라우팅 설정 및 프로젝트에서 필요한 객체들을 관리할 수 있도록 구현
    - 지금은 Controller 객체만 관리하고 있음
    - 나중에 Service, Repository 객체도 여기서 관리하게 변경할 예정
    - DispatcherServlet, AppConfig 추가
    - Controller 인터페이스 만듦

--- src/main/java/com/be24/api/board/BoardController.java
+++ src/main/java/com/be24/api/board/BoardController.java
@@ -2,35 +2,22 @@ package com.be24.api.board;
 
 import com.be24.api.board.model.BoardDto;
 import com.be24.api.common.BaseResponse;
+import com.be24.api.common.Controller;
 import com.be24.api.util.JsonParser;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-// web.xml에 등록하는 것 대신에 사용하는 Annotation
-@WebServlet(urlPatterns = {"/board/register"})
-public class BoardController extends HttpServlet {
-    // 클라이언트가 Http POST 방식으로 요청했을 때 실행되는 메소드
+// 이제 라우팅 처리를 DispatcherServlet & AppConfig가 해주기 때문에 주석 처리 (= 필요 없음)
+// @WebServlet(urlPatterns = {"/board/register"})
+public class BoardController implements Controller {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 요청으로 받은 데이터를 dto 객체 형태로 변환 (요청 데이터 -> 객체)
-        // 데이터 담는 부분을 DTO로 역할 분리, 데이터 전달 받아서 DTO에 담기
-        // JSON 형식으로 클라이언트가 전달한 요청을 처리
-        BoardDto dto = JsonParser.from(req, BoardDto.class);
+    public String process(HttpServletRequest req, HttpServletResponse resp) {
+        System.out.println("BoardController 실행 됨");
 
-        // 복잡한 로직을 처리하는 역할을 Service 클래스로 분리하고 메서드를 호출해서 사용
-        // boardService 클래스에 싱글톤 적용, new로 객체 생성 못하니까 메소드로 객체 가져옴
-        BoardService boardService = BoardService.getInstance();
-
-        // Service의 결과를 반환받게 변경
-        BoardDto returnDto = boardService.register(dto);
-
-        // 응답하는 부분, JSON 형식으로 응답을 처리
-        BaseResponse res = BaseResponse.success(returnDto);
-        resp.getWriter().write(JsonParser.from(res));  // HTTP 응답 Body에 글자를 쓰는 통로, 브라우저/Postman에서 보는 그 응답 내용
+        return "";
     }
 }
